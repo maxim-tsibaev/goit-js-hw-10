7e863050d6b686f33fd6af3f72cac30659204b33
maxim-tsibaev <m.tsibaev@gmail.com>
2023-04-01 18:59:36 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+async function fetchCountries() {
+  fetch('https://restcountries.com/v3.1/name/deutschland');
+  const response = await fetchCountries();
+  const data = await response.json();
+  console.log('🚀 ~ file: index.js:11 ~ data:', data);
+}
+
+fetchCountries();
